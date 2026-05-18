011908ad7f89ad0355997636f36ff0bdd3d6b8f2
Krzysztof Marszalek <krzys.marszalek@gmail.com>
2026-05-18 19:16:00 +0200

    Gunicorn and settings

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render provides a $PORT environment variable.
+    # If it's not found (like when running locally), it defaults to 5000.
+    port = int(os.environ.get("PORT", 5000))
+
+    # host='0.0.0.0' is required for Render to detect the service
+    app.run(host='0.0.0.0', port=port, debug=False)
